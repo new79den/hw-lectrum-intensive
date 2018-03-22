2ef4b60de284d8b28f37ffed6e2541fce2da2176
denis.korniychuk <new79den@gmail.com>
2018-03-22 20:48:07 +0200

    create site layout

--- src/components/Scheduler/index.js
+++ src/components/Scheduler/index.js
@@ -1,1 +1,45 @@
+import React, {Component} from "react"
+import Styles from "./styles.scss"
+import Checkbox from "../../theme/assets/Checkbox"
+import Task from "../Task/index"
+import propTypes from "prop-types"
 
+class Scheduler extends Component {
+    static propTypes = {}
+
+    render() {
+        return (
+            <div className={Styles.scheduler}>
+                <main>
+                    <header>
+                        <h1>Планировщик задач</h1>
+                        <input type="text"/>
+                    </header>
+                    <section>
+                        <form action="">
+                            <input type="text"/>
+                            <button>Добавить задачу</button>
+                        </form>
+
+                        <ul>
+                            <Task/>
+                            <Task/>
+                            <Task/>
+                        </ul>
+
+                    </section>
+
+                    <footer>
+
+                            <Checkbox color1="#000" color2="#f5f5f5"/>
+                            <span>Все задачи выполнены</span>
+
+                    </footer>
+                </main>
+
+            </div>
+        )
+    }
+}
+
+export default Scheduler
--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -1,1 +1,34 @@
+import React from "react"
+import Styles from "./styles.scss"
+import Checkbox from "../../theme/assets/Checkbox"
+import Star from "../../theme/assets/Star"
+import Edit from "../../theme/assets/Edit"
+import Delete from "../../theme/assets/Delete"
+import propoTypes from "prop-types"
 
+
+function Task(props) {
+
+    return (
+        <li className={Styles.task}>
+            <div>
+                <span>
+                    <Checkbox color1 = "#3B8EF3" color2 = "#fff"/>
+                </span>
+                <span>
+   Lorem ipsum dolor sit.
+                </span>
+            </div>
+
+
+            <div>
+                <Star color1 = "#3B8EF3" color2 = "#000"/>
+                <Edit color1 = "#3B8EF3" color2 = "#000"/>
+                <Delete color1 = "#3B8EF3" color2 = "#000"/>
+            </div>
+        </li>
+    )
+}
+
+
+export default Task;
